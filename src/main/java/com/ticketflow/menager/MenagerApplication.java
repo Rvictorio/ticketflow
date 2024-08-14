6395c79fac1edf11171894d6ec2caae785325a5d
Rvictorio <rvictorio8@gmail.com>
2024-08-14 12:31:59 -0300

    chore: setando o ambiente spring para inicio da aplicação

---
+++ src/main/java/com/ticketflow/menager/MenagerApplication.java
@@ -0,0 +1,13 @@
+package com.ticketflow.menager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticketflow/menager/MenagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketflow.menager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
